657d0c10a708e119bb72387c27107749550494bd
GunSik Choi <cgshome2@gmail.com>
2022-05-14 12:51:36 +0900

    add artifical sensors

--- src/car.js
+++ src/car.js
@@ -10,11 +10,14 @@ class Car {
         this.maxSpeed=3;
         this.friction=0.05;
         this.angle=0;
+
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
-    update() {
+    update(roadBorders) {
         this.#move();
+        this.sensor.update(roadBorders);
     }
 
     draw(ctx) {
@@ -32,6 +35,8 @@ class Car {
         ctx.fill();
         
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 
     #move() {
